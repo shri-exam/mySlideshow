f451c39b0d4709c8b47a2c94f1c7b5b6d6829144
m-peshekhonov <m-peshekhonov@tdtt-mac.local>
2013-03-27 21:34:39 +0200

    Починил стрелочки

--- desktop.blocks/box/box.js
+++ desktop.blocks/box/box.js
@@ -80,9 +80,19 @@ $.getJSON('http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?form
         return photoThumb;
     })();
 
-    photoThumb.bind('mousedown', function(){
+
+    function slider(param) {
         var photoBig = $('.box__photo-item img'),
-            _thisHash = $(this).attr('hash');
+            boxControls = (param.hasClass('box__control_direction_right'));
+
+            var _thisHash = boxControls ?
+                $('.box__mini_state_active').next().attr('hash') :
+                param.attr('hash');
+
+            boxControls && $('.box__mini_state_active')
+                            .removeClass('box__mini_state_active')
+                            .next()
+                            .addClass('box__mini_state_active');
 
         if ($('.stateActive').attr('id') !== _thisHash) {
             var photoBigActive = $('.stateActive');
@@ -90,32 +100,87 @@ $.getJSON('http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?form
             photoBigActive.animate({'left': '-' + photoBigActive.width()}, speed);
                 setTimeout(function() {
                     photoBigActive.removeAttr('style').removeAttr('class');
-                }, 600);
+                }, speed + 100);
+
+                activePhoto = $('.box__photo-item ').find('#'+_thisHash);
+                activePhoto
+                    .addClass('stateActive')
+                    .css({
+                        'margin-top': photoWrap.height()/2 - activePhoto.height()/2,
+                        'right': '-'+activePhoto.width()+'px'
+                    })
+                    .show()
+                    .animate({'right': photoWrap.width()/2 - activePhoto.width()/2}, speed);
+
+                setTimeout(function() {
+                        activePhoto.css({
+                            'right': '',
+                            'left':  photoWrap.width()/2 - activePhoto.width()/2
+                        });
+                }, speed);
+
+            $(window).resize(function(){
+                alignPhoto(activePhoto);
+            });
+
+            if(!boxControls) {
+                photoThumb.bind('click', function(){ return false; });
+                photoThumb.removeClass('box__mini_state_active');
+                param.addClass('box__mini_state_active');
+            }
+        }
+    }
+
+    photoThumb.bind('click', function(){
+        slider($(this));
+    });
+
+    $('.box__control_direction_right').bind('click', function(){
+        slider($(this));
+    });
+
+    $('.box__control_direction_left').bind('click', function(){
+
+        var activeF = $('.stateActive');
+        activeF.css({
+                    'left': '',
+                    'right':  photoWrap.width()/2 - activeF.width()/2
+                });
+
+        var photoBig = $('.box__photo-item img'),
+            _thisHash = $('.box__mini_state_active').prev().attr('hash');
+
+            $('.box__mini_state_active')
+                .removeClass('box__mini_state_active')
+                .prev()
+                .addClass('box__mini_state_active');
+
+        if ($('.stateActive').attr('id') !== _thisHash) {
+            var photoBigActive = $('.stateActive');
+
+            photoBigActive.animate({'right': '-' + photoBigActive.width()}, speed);
+            setTimeout(function() {
+                photoBigActive.removeAttr('style').removeAttr('class');
+            }, speed + 100);
 
             activePhoto = $('.box__photo-item ').find('#'+_thisHash);
             activePhoto
                 .addClass('stateActive')
                 .css({
                     'margin-top': photoWrap.height()/2 - activePhoto.height()/2,
-                    'right': '-'+activePhoto.width()+'px'
+                    'left': '-'+activePhoto.width()+'px'
                 })
                 .show()
-                .animate({'right': photoWrap.width()/2 - activePhoto.width()/2}, speed);
+                .animate({'left': photoWrap.width()/2 - activePhoto.width()/2}, speed);
 
             setTimeout(function() {
                     activePhoto.css({
-                        'right': '',
-                        'left':  photoWrap.width()/2 - activePhoto.width()/2
+                        'left': '',
+                        'right':  photoWrap.width()/2 - activePhoto.width()/2
                     });
-            }, speed + 100);
-
-            $(window).resize(function(){
-                alignPhoto(activePhoto);
-            });
-
-            photoThumb.removeClass('box__mini_state_active');
-            $(this).addClass('box__mini_state_active');
+            }, speed);
         }
+
     });
 
 // (function getTitle() {
