5233f660ad938038140f8b50fb449e5632cdc551
m-peshekhonov <m-peshekhonov@tdtt-mac.local>
2013-03-29 18:07:46 +0200

    Final version

--- desktop.blocks/box/box.js
+++ desktop.blocks/box/box.js
@@ -13,8 +13,8 @@ $(function() {
         progressbar = $('.progressbar__loading'),
         counterPhotos = $('.album__current-photo'),
         autoplay = $('.autoplay'),
-        setActive = 0;
-        $('.progressbar').fadeIn(800);
+        setActive = 0,
+        activeImg = $('.stateActive');
 
     function alignPhoto(param) {
         photoWrap.css('height', $(window).height());
@@ -26,33 +26,162 @@ $(function() {
 
     $(window).load(function() {
         progressbar.text('Всё готово!');
-        setTimeout(function () {
-           $('.progressbar').fadeOut(800);
-           loader.hide();
-        }, 1000);
+            setTimeout(function () {
+               $('.progressbar').fadeOut(800);
+               loader.hide();
+            }, 1000);
     });
 
     function scrollItems(elem, length) {
         ($(elem).hasClass('box__thumb-arrow_direction_right')) ?
-        scrollLength += length : scrollLength -= length;
-        scrollBar.animate({'scrollLeft': '+' + scrollLength}, speed);
+            scrollLength += length : scrollLength -= length;
+            scrollBar.animate({'scrollLeft': '+' + scrollLength}, speed);
     }
-
-function getAllPhotos(url) {
-    $.getJSON(url, function (data){
 
         function disableArrow() {
             if ( $('.box__mini_state_active').index() === 0 ) {
                 $('.box__control_direction_left').addClass('box__control_visibility_hidden');
             }
-            else if ( $('.box__mini_state_active').index() === data.entries.length - 1 ) {
+            else if ( $('.box__mini_state_active').index() === 99 ) {
                 $('.box__control_direction_right').addClass('box__control_visibility_hidden');
             }
             else {
-                $('.box__control_direction_left').removeClass('box__control_visibility_hidden');
-                $('.box__control_direction_right').removeClass('box__control_visibility_hidden');
-            }
+                $('.box__control_direction_left, .box__control_direction_right').removeClass('box__control_visibility_hidden');            }
         }
+
+    function slider(param) {
+            var photoBig = $('.box__photo-item img'),
+                boxCtrlRight = (param.hasClass('box__control_direction_right'));
+
+                if(boxCtrlRight) {
+                    param.addClass('box__control_disabled_yes');
+                        activeImg.css({
+                                'right': '',
+                                'left':  photoWrap.width()/2 - activeImg.width()/2
+                            });
+                } else {
+                    $('.box__mini').addClass('box__mini_disabled_yes');
+                }
+
+                var _thisHash = boxCtrlRight ?
+                    $('.box__mini_state_active').next().attr('hash') :
+                    param.attr('hash');
+                    setActive = Number(_thisHash);
+                    localStorage.setItem('active', setActive);
+
+                    counterPhotos.text(setActive + 1);
+
+                boxCtrlRight && $('.box__mini_state_active')
+                                .removeClass('box__mini_state_active')
+                                .next()
+                                .addClass('box__mini_state_active');
+
+                var newActiveImg = $('.stateActive');
+            if (newActiveImg.attr('id') !== _thisHash) {
+
+                newActiveImg.animate({'left': '-' + newActiveImg.width()}, speed);
+                    setTimeout(function() {
+                        newActiveImg.removeAttr('style').removeAttr('class');
+                    }, speed + 50);
+
+                    activePhoto = $('.box__photo-item ').find('#'+_thisHash);
+                    activePhoto
+                        .addClass('stateActive')
+                        .css({
+                            'margin-top': photoWrap.height()/2 - activePhoto.height()/2,
+                            'right': '-'+activePhoto.width()+'px'
+                        })
+                        .show()
+                        .animate({'right': photoWrap.width()/2 - activePhoto.width()/2}, speed);
+
+                    setTimeout(function() {
+                            activePhoto.css({
+                                'right': '',
+                                'left':  photoWrap.width()/2 - activePhoto.width()/2
+                            });
+                    }, speed);
+
+                $(window).resize(function(){
+                    alignPhoto(activePhoto);
+                });
+
+                if(!boxCtrlRight) {
+                    photoThumb.removeClass('box__mini_state_active');
+                    param.addClass('box__mini_state_active');
+                }
+            }
+
+            setTimeout(function() {
+                boxCtrlRight ? param.removeClass('box__control_disabled_yes') :
+                               $('.box__mini').removeClass('box__mini_disabled_yes');
+            }, speed);
+            disableArrow();
+        }
+
+    $('.box__control_direction_right').on('click', function(){
+            var controlRight = $(this);
+            if(!controlRight.hasClass('box__control_disabled_yes')) {
+                slider($(this));
+            }
+        });
+
+        $('.box__control_direction_left').on('click', function(){
+            var controlLeft = $(this);
+        if(!controlLeft.hasClass('box__control_disabled_yes')) {
+
+            controlLeft.addClass('box__control_disabled_yes');
+            activeImg.css({
+                        'left': '',
+                        'right':  photoWrap.width()/2 - activeImg.width()/2
+                    });
+
+            var photoBig = $('.box__photo-item img'),
+                _thisHash = $('.box__mini_state_active').prev().attr('hash');
+
+                setActive = Number(_thisHash);
+                localStorage.setItem('active', setActive);
+
+                counterPhotos.text(setActive + 1);
+
+                $('.box__mini_state_active')
+                    .removeClass('box__mini_state_active')
+                    .prev()
+                    .addClass('box__mini_state_active');
+
+            var newActiveImg = $('.stateActive');
+
+            if (newActiveImg.attr('id') !== _thisHash) {
+
+                newActiveImg.animate({'right': '-' + newActiveImg.width()}, speed);
+                setTimeout(function() {
+                    newActiveImg.removeAttr('style').removeAttr('class');
+                }, speed + 50);
+                activePhoto = $('.box__photo-item ').find('#'+_thisHash);
+                activePhoto
+                    .addClass('stateActive')
+                    .css({
+                        'margin-top': photoWrap.height()/2 - activePhoto.height()/2,
+                        'left': '-'+activePhoto.width()+'px'
+                    })
+                    .show()
+                    .animate({'left': photoWrap.width()/2 - activePhoto.width()/2}, speed);
+
+                setTimeout(function() {
+                        activePhoto.css({
+                            'left': '',
+                            'right':  photoWrap.width()/2 - activePhoto.width()/2
+                        });
+                }, speed);
+            }
+            setTimeout(function() {
+                controlLeft.removeClass('box__control_disabled_yes');
+            }, speed);
+                disableArrow();
+            }
+        });
+
+function getAllPhotos(url) {
+    $.getJSON(url, function (data){
 
         (function getTitle() {
             $('.album__title-name').text(data.title);
@@ -81,7 +210,6 @@ function getAllPhotos(url) {
                     .appendTo('.box__thumbs-list');
             }
             photoThumb = $('.box__mini');
-
             photoThumb.eq(itemActive).addClass('box__mini_state_active');
 
             return photoThumb;
@@ -110,153 +238,9 @@ function getAllPhotos(url) {
 
         })();
 
-        function slider(param) {
-            var photoBig = $('.box__photo-item img'),
-                boxControls = (param.hasClass('box__control_direction_right')),
-                controlRight = $('.box__control_direction_right');
-
-                if(!boxControls) {
-                    $('.box__mini').addClass('box__mini_disabled_yes');
-                }
-
-                if(boxControls) {
-                controlRight.addClass('box__control_disabled_yes');
-                var activeF = $('.stateActive');
-                    activeF.css({
-                            'right': '',
-                            'left':  photoWrap.width()/2 - activeF.width()/2
-                        });
-                }
-
-                var _thisHash = boxControls ?
-                    $('.box__mini_state_active').next().attr('hash') :
-                    param.attr('hash');
-
-                    setActive = Number(_thisHash);
-                    localStorage.setItem('active', setActive);
-
-                    counterPhotos.text(setActive + 1);
-
-                boxControls && $('.box__mini_state_active')
-                                .removeClass('box__mini_state_active')
-                                .next()
-                                .addClass('box__mini_state_active');
-
-            if ($('.stateActive').attr('id') !== _thisHash) {
-                var photoBigActive = $('.stateActive');
-
-                photoBigActive.animate({'left': '-' + photoBigActive.width()}, speed);
-                    setTimeout(function() {
-                        photoBigActive.removeAttr('style').removeAttr('class');
-                    }, speed + 50);
-
-                    activePhoto = $('.box__photo-item ').find('#'+_thisHash);
-                    activePhoto
-                        .addClass('stateActive')
-                        .css({
-                            'margin-top': photoWrap.height()/2 - activePhoto.height()/2,
-                            'right': '-'+activePhoto.width()+'px'
-                        })
-                        .show()
-                        .animate({'right': photoWrap.width()/2 - activePhoto.width()/2}, speed);
-
-                    setTimeout(function() {
-                            activePhoto.css({
-                                'right': '',
-                                'left':  photoWrap.width()/2 - activePhoto.width()/2
-                            });
-                    }, speed);
-
-                $(window).resize(function(){
-                    alignPhoto(activePhoto);
-                });
-
-                if(!boxControls) {
-                    photoThumb.on('click', function(){ return false; });
-                    photoThumb.removeClass('box__mini_state_active');
-                    param.addClass('box__mini_state_active');
-                }
-            }
-
-            if(boxControls) {
-                setTimeout(function() {
-                    controlRight.removeClass('box__control_disabled_yes');
-                }, speed);
-            } else {
-                setTimeout(function() {
-                    $('.box__mini').removeClass('box__mini_disabled_yes');
-                }, speed);
-            }
-            disableArrow();
-
-            return setActive;
-        }
-
         photoThumb.on('click', function(){
             if(!photoThumb.hasClass('box__mini_disabled_yes')) {
                 slider($(this));
-            }
-        });
-
-        $('.box__control_direction_right').on('click', function(){
-            var controlRight = $(this);
-            if(!controlRight.hasClass('box__control_disabled_yes')) {
-                slider($(this));
-            }
-        });
-
-        $('.box__control_direction_left').on('click', function(){
-            var controlLeft = $(this);
-        if(!controlLeft.hasClass('box__control_disabled_yes')) {
-
-            controlLeft.addClass('box__control_disabled_yes');
-            var activeF = $('.stateActive');
-            activeF.css({
-                        'left': '',
-                        'right':  photoWrap.width()/2 - activeF.width()/2
-                    });
-
-            var photoBig = $('.box__photo-item img'),
-                _thisHash = $('.box__mini_state_active').prev().attr('hash');
-
-                setActive = Number(_thisHash);
-                localStorage.setItem('active', setActive);
-
-                counterPhotos.text(setActive + 1);
-
-                $('.box__mini_state_active')
-                    .removeClass('box__mini_state_active')
-                    .prev()
-                    .addClass('box__mini_state_active');
-
-            if ($('.stateActive').attr('id') !== _thisHash) {
-                var photoBigActive = $('.stateActive');
-
-                photoBigActive.animate({'right': '-' + photoBigActive.width()}, speed);
-                setTimeout(function() {
-                    photoBigActive.removeAttr('style').removeAttr('class');
-                }, speed + 50);
-                activePhoto = $('.box__photo-item ').find('#'+_thisHash);
-                activePhoto
-                    .addClass('stateActive')
-                    .css({
-                        'margin-top': photoWrap.height()/2 - activePhoto.height()/2,
-                        'left': '-'+activePhoto.width()+'px'
-                    })
-                    .show()
-                    .animate({'left': photoWrap.width()/2 - activePhoto.width()/2}, speed);
-
-                setTimeout(function() {
-                        activePhoto.css({
-                            'left': '',
-                            'right':  photoWrap.width()/2 - activePhoto.width()/2
-                        });
-                }, speed);
-            }
-            setTimeout(function() {
-                controlLeft.removeClass('box__control_disabled_yes');
-            }, speed);
-                disableArrow();
             }
         });
 
@@ -286,7 +270,6 @@ function getAllPhotos(url) {
 
     });
 }
-
 getAllPhotos('http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?format=json&callback=?');
 
     $('.box__thumbs-list').on('mousewheel', function(e, delta) {
